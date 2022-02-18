513f6bba57bc2c55508797c6843011ed505320b7
Pieklonidas <tymoteuszkarwackip@gmail.com>
2022-02-18 21:31:43 +0100

    Wyrobienie BOVW
    Doszedłem do momentu, w którym nauczyłem moją sztuczną inteligencję BOVWa

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def get_train_df(ann_path, img_path):
     for a_path in ann_path_list:
         root = ET.parse(a_path).getroot()
         ann = {}
-        ann['filename'] = Path(str(img_path) + '/' + root.find("./filename").text)
+        ann['filename'] = cv2.imread(os.path.join((str(img_path) + root.find("./filename").text)))
         ann['width'] = root.find("./size/width").text
         ann['height'] = root.find("./size/height").text
         ann['class'] = root.find("./object/name").text
@@ -29,10 +29,48 @@ def get_train_df(ann_path, img_path):
         ann['xmax'] = int(root.find("./object/bndbox/xmax").text)
         ann['ymax'] = int(root.find("./object/bndbox/ymax").text)
         ann_list.append(ann)
-    return pandas.DataFrame(ann_list)
+    return ann_list
 
 
-df_train = get_train_df('annotations/', 'images/')
+def balance_dataset(data, ratio):
+    """
+    Subsamples dataset according to ratio.
+    @param data: List of samples.
+    @param ratio: Ratio of samples to be returned.
+    @return: Subsampled dataset.
+    """
+    sampled_data = random.sample(sorted(data), int(ratio * len(data)))
 
-print(df_train)
+    return sampled_data
 
+
+def learn_bovw(data):
+    """
+    Learns BoVW dictionary and saves it as "voc.npy" file.
+    @param data: List of dictionaries, one for every sample, with entries "image" (np.array with image) and "label" (class_id).
+    @return: Nothing
+    """
+    dict_size = 128
+    bow = cv2.BOWKMeansTrainer(dict_size)
+
+    sift = cv2.SIFT_create()
+    for sample in data:
+        kpts = sift.detect(sample['filename'], None)
+        kpts, desc = sift.compute(sample['filename'], kpts)
+
+        if desc is not None:
+            bow.add(desc)
+
+    vocabulary = bow.cluster()
+
+    np.save('voc.npy', vocabulary)
+
+
+print("Loading data")
+data_train = get_train_df('annotations/', 'images/')
+# print(data_train)
+# print("Balancing data")
+# data_train = balance_dataset(data_train, 1.0)
+
+# print("Learning BOVW")
+# learn_bovw(data_train)
