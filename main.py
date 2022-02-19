606efdaa5d361c03f72fc8e9799f65597b27133f
Pieklonidas <tymoteuszkarwackip@gmail.com>
2022-02-19 21:39:09 +0100

    Dzień 2
    Zrobiłem już prawie wszystko, brakuje mi tylko dodanie zbioru testowego, oraz tam innych mniejszych wymagań projektu

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import confusion_matrix
 import pandas
 
-
 def get_file_list(root, file_type):
     return [os.path.join(directory_path, f) for directory_path, directory_name,
             files in os.walk(root) for f in files if f.endswith(file_type)]
@@ -20,10 +19,14 @@ def get_train_df(ann_path, img_path):
     for a_path in ann_path_list:
         root = ET.parse(a_path).getroot()
         ann = {}
-        ann['filename'] = cv2.imread(os.path.join((str(img_path) + root.find("./filename").text)))
+        class_id = 0  # it's not crosswalk
+        ann['image'] = cv2.imread(os.path.join((str(img_path) + root.find("./filename").text)))
         ann['width'] = root.find("./size/width").text
         ann['height'] = root.find("./size/height").text
-        ann['class'] = root.find("./object/name").text
+        name = root.find("./object/name").text
+        if name == "crosswalk":
+            class_id = 1  # now it is crosswalk
+        ann['label'] = class_id
         ann['xmin'] = int(root.find("./object/bndbox/xmin").text)
         ann['ymin'] = int(root.find("./object/bndbox/ymin").text)
         ann['xmax'] = int(root.find("./object/bndbox/xmax").text)
@@ -39,7 +42,7 @@ def balance_dataset(data, ratio):
     @param ratio: Ratio of samples to be returned.
     @return: Subsampled dataset.
     """
-    sampled_data = random.sample(sorted(data), int(ratio * len(data)))
+    sampled_data = random.sample(data, int(ratio * len(data)))
 
     return sampled_data
 
@@ -55,8 +58,8 @@ def learn_bovw(data):
 
     sift = cv2.SIFT_create()
     for sample in data:
-        kpts = sift.detect(sample['filename'], None)
-        kpts, desc = sift.compute(sample['filename'], kpts)
+        kpts = sift.detect(sample['image'], None)
+        kpts, desc = sift.compute(sample['image'], kpts)
 
         if desc is not None:
             bow.add(desc)
@@ -66,11 +69,111 @@ def learn_bovw(data):
     np.save('voc.npy', vocabulary)
 
 
+def extract_features(data):
+    """
+    Extracts features for given data and saves it as "desc" entry.
+    @param data: List of dictionaries, one for every sample, with entries "image" (np.array with image) and "label" (class_id).
+    @return: Data with added descriptors for each sample.
+    """
+    sift = cv2.SIFT_create()
+    flann = cv2.FlannBasedMatcher_create()
+    bow = cv2.BOWImgDescriptorExtractor(sift, flann)
+    vocabulary = np.load('voc.npy')
+    bow.setVocabulary(vocabulary)
+    for sample in data:
+        kpts = sift.detect(sample['image'], None)
+        desc = bow.compute(sample['image'], kpts)
+        sample['desc'] = desc
+
+    return data
+
+
+def train(data):
+    """
+    Trains Random Forest classifier. #jak to zaimplementowac to random forest
+    #https://scikit-learn.org/stable/modules/generated/sklearn.ensemble.RandomForestClassifier.html
+    @param data: List of dictionaries, one for every sample, with entries "image" (np.array with image), "label" (class_id),
+                    "desc" (np.array with descriptor).
+    @return: Trained model.
+    """
+    descs = []
+    labels = []
+    for sample in data:
+        if sample['desc'] is not None:
+            descs.append(sample['desc'].squeeze(0))
+            labels.append(sample['label'])
+
+    rf = RandomForestClassifier()
+    rf.fit(descs, labels)
+
+    return rf
+
+
+def predict(rf, data):
+    """
+    Predicts labels given a model and saves them as "label_pred" (int) entry for each sample.
+    @param rf: Trained model.
+    @param data: List of dictionaries, one for every sample, with entries "image" (np.array with image), "label" (class_id),
+                    "desc" (np.array with descriptor).
+    @return: Data with added predicted labels for each sample.
+    """
+    for idx, sample in enumerate(data):
+        if sample['desc'] is not None:
+            pred = rf.predict(sample['desc'])
+            sample['label_pred'] = int(pred)
+
+    return data
+
+
+def evaluate(data):
+    """
+    Evaluates results of classification.
+    @param data: List of dictionaries, one for every sample, with entries "image" (np.array with image), "label" (class_id),
+                    "desc" (np.array with descriptor), and "label_pred".
+    @return: Nothing.
+    """
+    true = 0
+    false = 0
+    pred_labels = []
+    true_labels = []
+    for idx, sample in enumerate(data):
+        if sample['desc'] is not None:
+            pred_labels.append(sample['label_pred'])
+            true_labels.append(sample['label'])
+            if sample['label_pred'] == sample['label']:
+                true += 1
+            else:
+                false += 1
+
+    accuracy = true / (true + false)
+    print("Accuracy: " + str(accuracy))
+
+    conf_mat = confusion_matrix(true_labels, pred_labels)
+    print(conf_mat)
+    # this function does not return anything
+    return
+
+
 print("Loading data")
-data_train = get_train_df('annotations/', 'images/')
+data_train = get_train_df('train/annotations/', 'train/images/')
+data_test = get_train_df('train/annotations/', 'train/images/')
 # print(data_train)
-# print("Balancing data")
-# data_train = balance_dataset(data_train, 1.0)
+print("Balancing data")
+data_train = balance_dataset(data_train, 1.0)
 
 # print("Learning BOVW")
 # learn_bovw(data_train)
+
+print('extracting train features')
+data_train = extract_features(data_train)
+
+print('training')
+rf = train(data_train)
+
+print('extracting test features')
+data_test = extract_features(data_test)
+
+print('testing on testing dataset')
+data_test = predict(rf, data_test)
+evaluate(data_test)
+
